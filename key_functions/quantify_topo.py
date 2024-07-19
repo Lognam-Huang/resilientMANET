3ebfb1df860ca37df9b3aaf64a3a286b96dc7db5
Y9000P <lognamhuang@gmail.com>
2024-07-19 12:15:28 -0400

    Solving BP problem, not yet

--- key_functions/quantify_topo.py
+++ key_functions/quantify_topo.py
@@ -533,7 +533,7 @@ def quantify_backup_path_with_GU(ground_users, gu_to_uav_connections, UAVMap, ho
     print("zz")
     print(total_score / filtered_path_count)
     print(filtered_path_count / total_path_count)
-    print("xx")
+    print("xxx")
 
     score = 0 if filtered_path_count == 0 else (total_score / filtered_path_count) * (filtered_path_count / total_path_count)
     return score
